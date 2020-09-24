fdc0ffce5f6afb55f1745172b3f07b8c38224a5f
isphinx <isphinx@me.com>
2020-09-24 16:56:26 +1000

    Initial Commit

---
+++ HSB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HSB
+//
+//  Created by lucas lee on 24/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HSB/HSBApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSBApp.swift
+//  HSB
+//
+//  Created by lucas lee on 24/9/20.
+//
+
+import SwiftUI
+
+@main
+struct HSBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
